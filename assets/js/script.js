05b4564ab595949cfd2881a041a92876d8157f16
Ridercogswell <ridercogswell@gmail.com>
2022-02-07 14:28:36 -0800

    youtube api working

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,100 @@ var searchControl = new L.esri.Controls.Geosearch().addTo(map);
                   results.addLayer(L.marker(data.results[i].latlng));
                 }
               });
+
+
+
+// Yelp Api 
+
+var searchFormEl = document.querySelector("#city-form")
+var youtubeInputEl = document.querySelector("#music-type");
+var mapInputEl = document.querySelector("#city-name");
+var youtubeContainerEL = document.querySelector("#youtube");
+
+// handle form submission
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+
+// var cityName = mapInputEl.value.trim();
+  var genreName = youtubeInputEl.value.trim();
+
+  if (genreName) {
+    getYoutubeData(genreName);
+    youtubeInputEl.value = "";
+    mapInputEl.value = "";
+  } else {
+    alert("Please enter a valid response")
+  }
+
+  console.log(event);
+}
+
+// display data to doc
+var displayYoutube = function(videos, searchTerm) {
+  var videosItem = videos.items;
+  if (videos.length === 0) {
+    youtubeContainerEL.textContent = "No videos found!";
+    return;
+  }
+
+  youtubeContainerEL.textContent = "";
+  
+  for (var i = 0; i < videosItem.length; i++) {
+    // set variables
+    var videoName = videosItem[i].snippet.title;
+    var channelName = videosItem[i].snippet.channelTitle;
+    var videoThumbnail = videosItem[i].snippet.thumbnails.default.url;
+    var videoLink = videosItem[i].id.videoId;
+    console.log(videoName, channelName, videoThumbnail);
+
+    // create and print elements to page
+    var vidContainerEl = document.createElement("a");
+    vidContainerEl.classList = "card cell small-2 margin-top";
+    vidContainerEl.setAttribute("href", "https://www.youtube.com/watch?v=" + videoLink);
+
+    youtubeContainerEL.appendChild(vidContainerEl);
+    // create span el to hold video name, channel name, and img
+    titleEl = document.createElement("h5");
+    titleEl.textContent = videoName;
+    vidImgEl = document.createElement("img");
+    vidImgEl.setAttribute("src", videoThumbnail);
+    channelNameEl = document.createElement("h5");
+    channelNameEl.textContent = channelName;
+
+    // append to container
+    vidContainerEl.appendChild(titleEl);
+    vidContainerEl.appendChild(vidImgEl);
+    vidContainerEl.appendChild(channelNameEl);
+
+  }
+}
+
+// get data from yelp api
+var getYoutubeData = function(category) {
+  // format yelp api url
+  var apiUrl = "https://youtube.googleapis.com/youtube/v3/search?q=" + category + "&part=snippet&key=AIzaSyAOQbpKtfxhVRprkzvTqdOdbcADvgO2CI8";
+
+  // make request to url with nested then function
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayYoutube(data, category);
+        console.log(data, category);
+      });
+    } else {
+      alert("Error: YouTube Users Not Found")
+    }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to YouTube");
+  });
+
+  console.log(category);
+};
+
+// getYoutubeData();
+
+
+// add event listeners
+searchFormEl.addEventListener("submit", formSubmitHandler)
