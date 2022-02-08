34fe5ae9defd3e4a11aa57ae338049f45f640c51
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-02-08 14:33:11 -0800

    added search bar to map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // Map Variable
 var map = L.map('map').setView([38.58, -121.5], 13);
+//City Variable
+var cityNameEl = document.querySelector("#city-name");
 
 // Map 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -10,17 +12,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibXJwcm90c3l1ayIsImEiOiJja3o3dnhpeHIwZ3g1Mm9tbXdsZTRsY3IzIn0.Lrx09QwyMOJGTdqtHrE3eg'
 }).addTo(map);
+// Map Search
+L.Control.geocoder().addTo(map);
 
-// Map Search Bar
-L.control.scale().addTo(map);
-
-var searchControl = new L.esri.Controls.Geosearch().addTo(map);
-
-         var results = new L.LayerGroup().addTo(map);
-
-             searchControl.on('results', function(data){
-                results.clearLayers();
-                for (var i = data.results.length - 1; i >= 0; i--) {
-                  results.addLayer(L.marker(data.results[i].latlng));
-                }
-              });
