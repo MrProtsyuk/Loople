06c315da944e8600acd95116fb083832cb857c00
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-02-08 19:44:59 -0800

    accidental changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,26 +89,21 @@ var getYoutubeData = function(category) {
 
 // getYoutubeData();
 
+//News API
+var url = 'https://newsapi.org/v2/everything?' + category + 'from=2022-02-08&' + 'sortBy=popularity&' + 'apiKey=64182e052fd1413bba8aa03676db4aa2';
+
+var req = new Request(url);
+
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
+
 
 // add event listeners
 searchFormEl.addEventListener("submit", formSubmitHandler)
-
 // Map Variable
 var map = L.map('map').setView([38.58, -121.5], 13);
 //City Variable
 var cityNameEl = document.querySelector("#city-name");
 
-// Map 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibXJwcm90c3l1ayIsImEiOiJja3o3dnhpeHIwZ3g1Mm9tbXdsZTRsY3IzIn0.Lrx09QwyMOJGTdqtHrE3eg'
-}).addTo(map);
-// Map Search
-L.Control.geocoder().addTo(map);
-
-// Map Search Bar
-L.control.scale().addTo(map);
